0df8d4b3a0c3902435e500e4e64de3f01d9b27aa
Tammy Tian <tiantammy@outlook.com>
2018-05-23 22:38:41 +0200

    editied on submit to set a value for issue

--- src/components/ComplaintForm.js
+++ src/components/ComplaintForm.js
@@ -1,38 +1,114 @@
-import React, { Component } from 'react';
-import {reduxForm, Field, focus} from 'redux-form';
-//import './ComplaintForm.css';
+import React from 'react';
+import {reduxForm, Field, focus, SubmissionError} from 'redux-form';
+import './ComplaintForm.css';
 import Input from './Input';
-import {required, nonEmpty, email} from '../validator';
+import {required, nonEmpty, trackingNumber, trackingNumberLength} from '../validator';
 
 export class ComplaintForm extends React.Component {
-  // onSubmit(values) {
-  //   return fetch();
-  
+  onSubmit(values) {
+    if (!values.issue) {
+      values.issue = "not-delivered";
+    } 
+    return fetch('https://us-central1-delivery-form-api.cloudfunctions.net/api/report', {
+        method: 'POST',
+        body: JSON.stringify(values),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(res => {
+      if (!res.ok) {
+          if (
+              res.headers.has('content-type') &&
+              res.headers
+                  .get('content-type')
+                  .startsWith('application/json')
+          ) {
+              // It's a nice JSON error returned by us, so decode it
+              return res.json().then(err => Promise.reject(err));
+          }
+          // It's a less informative error returned by express
+          return Promise.reject({
+              code: res.status,
+              message: res.statusText
+          });
+      }
+      return;
+  })
+  .then(() => console.log('Submitted with values', values))
+  .catch(err => {
+      const {reason, message, location} = err;
+      if (reason === 'ValidationError') {
+          // Convert ValidationErrors into SubmissionErrors for Redux Form
+          return Promise.reject(
+              new SubmissionError({
+                  [location]: message
+              })
+          );
+      }
+      return Promise.reject(
+          new SubmissionError({
+              _error: 'Error submitting message'
+          })
+      );
+  });
+}
+
   render() {
+
+      let successMessage;
+      if (this.props.submitSucceeded) {
+          successMessage = (
+              <div className="message message-success">
+                  Message submitted successfully
+              </div>
+          );
+      }
+  
+      let errorMessage;
+      if (this.props.error) {
+          errorMessage = (
+              <div className="message message-error">{this.props.error}</div>
+          );
+      }
+
     return (
-      <div>
-        <h2> Report a problem with your delivery </h2>
-        <form>
+       
+        <form onSubmit={this.props.handleSubmit(values =>
+            this.onSubmit(values)
+        )}>
+            {successMessage}
+            {errorMessage}
+           <h2> Report a problem with your delivery </h2>
           <Field 
             name="trackingNumber"
             type="text"
             label="Tracking Number"
             component={Input}
-            validate={[required, nonEmpty]}
+            validate={[required, nonEmpty, trackingNumber, trackingNumberLength]}
           />
-          <Field
-            name="issueSelectorr"
-            element="select"
-            label="What is your issue?"
-            component={Input}
-            validate={[required, nonEmpty]}
-          />
+
+          <div>
+            <Field
+              name="issue"
+              id="issue"
+              component="select"
+              label="What is your issue?"
+            >            
+              <option value="not-delivered">My delivery hasn't arrived</option>
+              <option value="wrong-item">The wrong item was delivered</option>
+              <option value="missing-part">Part of my order was missing</option>
+              <option value="damaged">Some of my order arrived damaged</option>
+              <option value="other">Other (give details below)</option>
+
+              {/* better to use obj (key value pair {not-delivered: ...} /array */}
+            </Field>
+          </div>
           <Field
             name="details"
             label="Give more details (optional)"
             element="textarea"
             component={Input}
-            validate={[required, nonEmpty]}
           />
           <button 
             type="submit"
@@ -40,7 +116,6 @@ export class ComplaintForm extends React.Component {
             Submit
           </button>
         </form>
-      </div>
     );
   }
 }
