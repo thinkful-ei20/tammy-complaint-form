f4d6c25d8d26f68518691832cd784765bdfe9f39
Tammy Tian <tiantammy@outlook.com>
2018-05-23 22:37:29 +0200

    added redux dev-tools

--- src/store.js
+++ src/store.js
@@ -4,5 +4,6 @@ import {reducer as formReducer} from 'redux-form';
 export default createStore(
   combineReducers({
     form: formReducer
-  })
+  }),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
