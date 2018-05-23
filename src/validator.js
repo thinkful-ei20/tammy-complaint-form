fbf6f14536135b4c0c15d19064b1175074129b72
Tammy Tian <tiantammy@outlook.com>
2018-05-23 22:36:50 +0200

    added validators for tracking number

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,5 @@
 export const required = value => (value ? undefined : ' Required');
 export const nonEmpty = value => 
   value.trim() !== '' ? undefined : 'Cannot be empty';
-export const email = value =>
-/^\S+@\S+$/.test(value) ? undefined : 'Must be a valid email address';
+export const trackingNumberLength = value =>(value.trim().length === 5 ? undefined : 'Tracking number must be 5 characters long');
+export const trackingNumber = value => (value.match(/^-{0,1}\d+$/) ? undefined : 'Only Numbers Allowed');
